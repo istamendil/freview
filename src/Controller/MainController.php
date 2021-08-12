56459b22526bb40f80adea7bcded28047828c24b
Alexander Ferenets <ist.kazan@gmail.com>
2021-08-12 17:16:12 +0300

    User base: authentication, registration, profile page

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,7 +8,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\ParameterBag\ContainerBagInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
+/**
+ * Some bin controller: actions goes here before getting own controller due to too many actions of one logic group.
+ */
 class MainController extends AbstractController
 {
     /**
@@ -25,6 +29,15 @@ class MainController extends AbstractController
     public function second(ContainerBagInterface $c)
     {
         return $this->render('second.html.twig');
+    }
+
+    /**
+     * @Route("/secret_admin", name="secret_admin")
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function secretAdmin(ContainerBagInterface $c): Response
+    {
+        return $this->render('secret_admin.html.twig');
     }
 
 }
--- src/DataFixtures/BaseFixture.php
+++ src/DataFixtures/BaseFixture.php
@@ -2,16 +2,36 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 /**
  * Here should be everything that may be needed at site starting to work point.
  */
 class BaseFixture extends Fixture
 {
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public function load(ObjectManager $manager)
     {
-        //$manager->flush();
+        $this->loadUsers($manager);
+    }
+
+    protected function loadUsers(ObjectManager $manager){
+        $user = new User();
+        $user->setEmail('admin@example.com');
+        $user->setFullname('John Doe');
+        $user->setNickname('Admin');
+        $user->setRoles(['ROLE_ADMIN']);
+        $user->setPassword($this->passwordHasher->hashPassword($user,'admin'));
+        $manager->persist($user);
+        $manager->flush();
     }
 }
